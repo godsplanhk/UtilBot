8bb32086d813302542d818a114aea99e66b0b896
Hare Krishna Raj <godsplanhk@gmail.com>
2025-02-17 10:53:31 +0530

    docker image generatation optimization

--- utils/ocr_doc.py
+++ utils/ocr_doc.py
@@ -3,7 +3,12 @@ from PyPDF2 import PdfReader
 import os
 import docx
 
+WNS_COLS_NUM = "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}num"
 
+
+def set_number_of_columns(section, cols):
+    """ sets number of columns through xpath. """
+    section._sectPr.xpath("./w:cols")[0].set(WNS_COLS_NUM, str(cols))
 def qExtractor(inputPdf):
     count = 0
     if not os.path.isfile("./in/OCR/" + inputPdf):
@@ -13,17 +18,20 @@ def qExtractor(inputPdf):
             language=["hin", "eng"],
         )
     else:
-        return "./out/" + inputPdf.split('.')[0]+".docx"
+        if os.path.isfile("./out/"+inputPdf.split('.')[0]+'.docx'):
+            return "./out/" + inputPdf.split('.')[0]+".docx"
     reader = PdfReader("./in/OCR/" + inputPdf)
     f = docx.Document()
     f.styles["Normal"].font.name = "Arial"
+    paragraph_format = f.styles['Normal'].paragraph_format
+    paragraph_format.line_spacing = 1
     print(len(reader.pages))
-    page = reader.pages[5]
-    text = page.extract_text()
+    section = f.sections[0]
+    set_number_of_columns(section, 2)
     for page in reader.pages:
         count += 1
         text = page.extract_text()
-        out = "\n\n" + str(count) + ") " + text.rstrip()
+        out =  str(count) + ") " + text.rstrip()
         print(out)
         f.add_paragraph(out)
     f.save("./out/" + inputPdf.split(".")[0] + ".docx")
